dc60c6338c0c67ea74bac9fcbd83dcd01e78b8ac
Dave Havelaar <2112137@talnet.nl>
2024-09-03 14:46:16 +0200

    Maak opdracht Statements

---
+++ PROG1/PROG1.3/Statements.py
@@ -0,0 +1,15 @@
+import statistics
+
+a = int(6)
+b = int(7)
+c = statistics.mean([a, b])
+
+print(c)
+
+voornaam = str("Dave")
+tussenvoegsel = str("")
+achternaam = str("Havelaar")
+
+mijnnaam = voornaam + " " + tussenvoegsel + " " + achternaam
+
+print(mijnnaam)
