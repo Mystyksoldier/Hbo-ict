65d2c3c11071253eeabf09b601415d1d03d34f15
Dave Havelaar <2112137@talnet.nl>
2024-09-08 17:31:05 +0200

    PROG1.7

--- PROG1/PROG1.6/list&numbers.py
+++ PROG1/PROG1.6/list&numbers.py
@@ -1,2 +1,10 @@
-numberList = int(14, 8, -22)
+import math
 
+numberList = [3, 7, -2, 12]
+
+min_value = min(numberList)
+max_value = max(numberList)
+
+distance = abs(max_value - min_value)
+
+print(distance)
---
+++ PROG1/PROG1.7/tuples.py
@@ -0,0 +1,7 @@
+letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+
+tuplesToList = list(letters)
+
+result = [tuplesToList.count('A'), tuplesToList.count('B'), tuplesToList.count('C')]
+
+print(result)
