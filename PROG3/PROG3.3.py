013f576ee9a192b31310aee477c886d76f65ee09
Dave Havelaar <2112137@talnet.nl>
2024-09-18 12:48:42 +0200

    feedback PROG3.3

--- PROG3/PROG3.3.py
+++ PROG3/PROG3.3.py
@@ -1,13 +1,13 @@
 montNumber = int(input("geef het nummer van de maand(tussen 1 en 12): "))
 
-months_dutch = ["januari", "februari",  "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"]
+monthsDutch = ["januari", "februari",  "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"]
 
 if montNumber >= 1 and montNumber <= 12:
-  if montNumber >=3 and montNumber <=5:
+  if montNumber >= 3 and montNumber <= 5:
     print("lente")
   elif montNumber >= 6 and montNumber <= 8:
     print("zomer")
-  elif montNumber >= 9 and months_dutch <= 11:
+  elif montNumber >= 9 and monthsDutch <= 11:
     print("herfst")
   else:
     print("winter")
