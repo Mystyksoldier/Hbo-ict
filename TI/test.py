4deecfa773836c6170515e559e64de2f9da2a9c3
Dave Havelaar <2112137@talnet.nl>
2024-11-07 15:35:31 +0100

    ai

--- TI/test.py
+++ TI/test.py
@@ -1,15 +1,11 @@
-from machine import Pin
-import time
 
-led_pins = [Pin(0, Pin.OUT), Pin(1, Pin.OUT), Pin(2, Pin.OUT), Pin(3, Pin.OUT)]
+n = 3
 
-while True:
-    # Turn on all LEDs
-    for led in led_pins:
-        led.value(1)
-    time.sleep(0.5)
 
-    # Turn off all LEDs
-    for led in led_pins:
-        led.value(0)
-    time.sleep(0.5)
+binary_str = bin(n)[2:]
+
+result = tuple(int(bit) for bit in binary_str)
+
+print(result)
+
+
